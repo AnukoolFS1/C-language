72392fabdcebc1abdfc97c07e6cc618c906820f2
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-06-11 21:26:38 +0530

    basic output and datatypes

---
+++ 01_basics/dataTypes_variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    float height = 3.9;
+    char Grade = 'A';
+
+    printf("Age %d\n", age);
+    printf("Height %.1f\n", height);
+    printf("Grade %c\n", Grade);
+
+    return 0;
+}
