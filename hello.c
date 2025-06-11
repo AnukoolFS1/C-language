9bb9595dd6a6c27ca49816ff34db259bc41b6a5c
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-06-11 21:01:50 +0530

    starting back the C language from the beginning

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h> // preprocessor directive
-
-int main() { // Main function Entry point
-    printf("Hello! we are learning C-language.\n"); // not impliciting the \n will cause "%"" char in the last of printed line
-    return 0; // return statement
-}
