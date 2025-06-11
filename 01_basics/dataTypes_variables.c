a476bb396e772e7c89c3061d13bdba824d7435dd
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-06-11 21:52:44 +0530

    practicing the print and double

--- 01_basics/dataTypes_variables.c
+++ 01_basics/dataTypes_variables.c
@@ -4,10 +4,12 @@ int main() {
     int age = 25;
     float height = 3.9;
     char Grade = 'A';
+    double PI = 3523.14159265358979323846;
 
     printf("Age %d\n", age);
-    printf("Height %.1f\n", height);
+    printf("Height %.15f\n", height);
     printf("Grade %c\n", Grade);
+    printf("Double precision %.20f\n", PI);
 
     return 0;
 }
